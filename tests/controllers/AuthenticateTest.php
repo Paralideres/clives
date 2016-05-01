94e85d67f540ce2ab560316681337a8497f63556
jvivas <jvivas@climate.com>
2016-05-01 06:51:48 -0700

    Implement a unit test

---
+++ tests/controllers/AuthenticateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthenticateTest extends TestCase
+{
+    /**
+     * Test the authenticate route response
+     *
+     * @return void
+     */
+    public function testAuthenticate()
+    {
+      $response = $this->call('POST', 'api/v1/authenticate', ['email' => 'jorge@paralideres.org', 'password' => 'test123']);
+      $object = json_decode($response->getContent());
+
+      $this->assertNotNull($object->token);
+      $this->assertInternalType('string', $object->token);
+    }
+}
